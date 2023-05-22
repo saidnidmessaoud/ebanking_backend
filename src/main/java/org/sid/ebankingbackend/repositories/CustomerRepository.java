b68675df6292237745d1511e283914232fd31570
saidnidmessaoud <nidmessaoudsaid1@gmail.com>
2023-05-22 23:00:53 +0100

    second commit on the bank project

---
+++ src/main/java/org/sid/ebankingbackend/repositories/CustomerRepository.java
@@ -0,0 +1,8 @@
+package org.sid.ebankingbackend.repositories;
+
+import org.sid.ebankingbackend.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+}
